8ec885f9c7fb22fe455ac0717232857d8ed0dee8
visarmec-rgb <visarmec@uw.edu>
2026-06-03 11:51:31 -0700

    Changed the Dealer to JSON

--- dealer/DealerClient.java
+++ dealer/DealerClient.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.Random;
-
 import Card_And_Message_Stuff.Card;
 import Card_And_Message_Stuff.Message;
 import Card_And_Message_Stuff.MessageType;
@@ -9,28 +8,49 @@ import Card_And_Message_Stuff.Rank;
 import Card_And_Message_Stuff.Suit;
 
 public class DealerClient {
+
     public void start() throws IOException {
         Socket socket = new Socket("localhost", 2121);
-        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+        
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())
+        );
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
         Random random = new Random();
 
-        while (true) {
-            Message message = (Message) in.readObject();
+       
+        out.println("{\"sender\":\"DEALER\"}");
 
-            if (message.getType() == MessageType.Request_Card) {
+        while (true) {
+            String line = in.readLine();
+            if (line == null) break;
+
+           
+            if (line.contains("\"PLAYER_ACTION\"")) {
+
+               
                 Rank[] ranks = Rank.values();
                 Suit[] suits = Suit.values();
                 Rank rank = ranks[random.nextInt(ranks.length)];
                 Suit suit = suits[random.nextInt(suits.length)];
                 Card card = new Card(rank, suit);
 
-                Message response = new Message(MessageType.Deal_Card, "Dealer", message.getSender(),
-                                                card.toString());
-                out.writeObject(response);
-                out.flush();
+                // Send JSON back to server
+                String response = "{\n" +
+                        "  \"type\": \"DEAL_CARD\",\n" +
+                        "  \"sender\": \"DEALER\",\n" +
+                        "  \"target\": \"PLAYER\",\n" +
+                        "  \"payload\": {\n" +
+                        "    \"card\": \"" + card.toString() + "\"\n" +
+                        "  }\n" +
+                        "}";
+
+                out.println(response);
             }
         }
+
         socket.close();
     }
 }
